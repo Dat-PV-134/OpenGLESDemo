5e6a43067617eb93c85160112eba11e0eee95a89
Phạm Văn Đạt <94840545+Dat-PV-134@users.noreply.github.com>
2024-07-01 18:14:13 +0700

    implement free learning resoures

--- app/src/main/java/com/rekoj134/opengldemo/MainActivity.kt
+++ app/src/main/java/com/rekoj134/opengldemo/MainActivity.kt
@@ -2,14 +2,12 @@ package com.rekoj134.opengldemo
 
 import android.opengl.GLSurfaceView
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.rekoj134.opengldemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var myRenderer: MyGLRender
+    private lateinit var myRenderer: MyGLRenderer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +15,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.myGlSurfaceView.setEGLContextClientVersion(2)
-        myRenderer = MyGLRender()
+        myRenderer = MyGLRenderer()
         binding.myGlSurfaceView.setRenderer(myRenderer)
+        binding.myGlSurfaceView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+    }
+
+    override fun onResume() {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume()
+        binding.myGlSurfaceView.onResume()
+    }
+
+    override fun onPause() {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause()
+        binding.myGlSurfaceView.onPause()
     }
 }
