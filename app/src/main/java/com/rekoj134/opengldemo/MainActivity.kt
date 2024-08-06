8034f6bd0fb30f330bf85926572de1987df8c3ca
Phạm Văn Đạt <94840545+Dat-PV-134@users.noreply.github.com>
2024-08-06 11:39:30 +0700

    Done lession 1

--- app/src/main/java/com/rekoj134/opengldemo/MainActivity.kt
+++ app/src/main/java/com/rekoj134/opengldemo/MainActivity.kt
@@ -6,29 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.rekoj134.opengldemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var myRenderer: MyGLRenderer
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.myGlSurfaceView.setEGLContextClientVersion(2)
-        myRenderer = MyGLRenderer()
-        binding.myGlSurfaceView.setRenderer(myRenderer)
-        binding.myGlSurfaceView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
-    }
-
-    override fun onResume() {
-        // The activity must call the GL surface view's onResume() on activity onResume().
-        super.onResume()
-        binding.myGlSurfaceView.onResume()
-    }
-
-    override fun onPause() {
-        // The activity must call the GL surface view's onPause() on activity onPause().
-        super.onPause()
-        binding.myGlSurfaceView.onPause()
     }
 }
---
+++ app/src/main/java/com/rekoj134/opengldemo/from_book/first_lesson/FistLessonRenderer.kt
@@ -0,0 +1,23 @@
+package com.rekoj134.opengldemo.from_book.first_lesson
+
+import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
+import android.opengl.GLES20.glClear
+import android.opengl.GLES20.glClearColor
+import android.opengl.GLES20.glViewport
+import android.opengl.GLSurfaceView.Renderer
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class FistLessonRenderer : Renderer {
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        glClearColor(1.0f, 0f, 0f, 0f)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        glViewport(0, 0, width, height)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        glClear(GL_COLOR_BUFFER_BIT)
+    }
+}
